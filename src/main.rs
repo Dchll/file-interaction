0bec5fb4ec963e8d7bb5c11bc50fe206560e35fb
多吃韩66 <15535517032@163.com>
2025-02-27 13:25:50 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ struct File {
 impl File {
     fn new(name: &str) -> Self {
         let name = name.to_string();
-        let data = Vec::<u8>::new();
+        // let data = Vec::<u8>::new();
+        let data = vec![114, 117, 115, 116, 33];
         Self { name, data }
     }
 
@@ -22,15 +23,27 @@ impl File {
         true
     }
 
-    #[allow(unused)]
-    fn read(&mut self) -> ! {
-        todo!()
+    fn read_to(&self, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = self.data.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+
+        read_length
     }
 }
 
 fn main() {
     let mut f1 = File::new("f1.txt");
+    let mut buffer = Vec::<u8>::new();
+
     f1.open();
-    f1.read();
+    let f1_len = f1.read_to(&mut buffer);
     f1.close();
+
+    let text = String::from_utf8_lossy(&buffer).to_string();
+
+    println!("f1: {:?}", f1);
+    println!("text: {} ({} Bytes)", text, f1_len);
 }
