435d4ffc84116196df18633277a626c2072b4050
dch66 <15535517032@163.com>
2025-02-27 10:28:11 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused_variables)]
+#![allow(unreachable_code)]
+
+type File = String;
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+#[allow(unused)]
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f1 = File::from("f1.txt");
+    let mut data_f1 = Vec::new();
+    open(&mut f1);
+    read(&mut f1, &mut data_f1);
+    close(&mut f1);
 }
